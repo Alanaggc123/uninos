b14e24bc4cf0f0e4d7210d25adc223ad5d62d81d
Alanaggc123 <alanaggchagas@edu.unirio.br>
2025-11-25 13:32:26 -0300

    Add files via upload

---
+++ backend/prisma/migrations/20251125023335_init/migration.sql
@@ -0,0 +1,16 @@
+-- CreateTable
+CREATE TABLE `User` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `email` VARCHAR(191) NOT NULL,
+    `passwordHash` VARCHAR(191) NOT NULL,
+    `fullName` VARCHAR(191) NULL,
+    `gender` VARCHAR(191) NULL,
+    `matricula` VARCHAR(191) NULL,
+    `curso` VARCHAR(191) NULL,
+    `periodo` INTEGER NULL,
+    `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `updatedAt` DATETIME(3) NOT NULL,
+
+    UNIQUE INDEX `User_email_key`(`email`),
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
