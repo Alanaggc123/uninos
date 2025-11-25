425aab42e4260d6787f44c4772730af1ab09ae44
Alanaggc123 <alanaggchagas@edu.unirio.br>
2025-11-25 13:52:42 -0300

    Add files via upload

---
+++ frontend/dev/static/chunks/app_auth_sign-up_page_tsx_6bece898._.js
@@ -0,0 +1,8 @@
+(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/node_modules_2b0e89db._.js",
+  "static/chunks/_b4decbcc._.js"
+],
+    source: "dynamic"
+});
